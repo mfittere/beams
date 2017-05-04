c778aa6fe797d246945a16e209cc032d62b841ea
mfittere <miriam.fitterer@cern.ch>
2017-05-04 11:03:20 -0500

    add ideal kick from elens

--- elens.py
+++ elens.py
@@ -5,7 +5,8 @@ import numpy as np
 
 def hel_thetamax(r2,I,l,EkeV_e,EGeV_p,direction='opposite'):
   """
-  calculate maximum kick from ideal e-lens:
+  calculate maximum kick from uniform distribution
+  in r=sqrt(x**2+y**2) for e-lens:
   
   Parameters:
   -----------
@@ -34,3 +35,24 @@ def hel_thetamax(r2,I,l,EkeV_e,EGeV_p,direction='opposite'):
   if (direction=='same'):
     return 2*l*I*(1-beta_e*beta_p)/(4*np.pi*eps0_vacuum*brho_p*beta_e
                                     *beta_p*clight**2)*1/r2
+
+def hel_kick(r,r1,r2,thetamax):
+  """
+  return the kick from uniform distribution
+  in r=sqrt(x**2+y**2) for e-lens:
+
+  Parameters:
+  -----------
+  r        : proton particle position r=sqrt(x**2+y**2)
+  r1       : inner radius [m]
+  r2       : outer radius [m]
+  thetamax : maximum kick [m] (see hel_thetamax)
+  """
+  # project -r on r
+  r = np.abs(r)
+  # r > r2 -> kick =1
+  kick = np.where(r>=r2,1,0)
+  # overwrite intermediate region
+  kick_aux = np.where((r>r1) & (r<r2),(r**2-r1**2)/(r2**2-r1**2),0)
+  return (kick+kick_aux)*(r2/r)*thetamax
+
